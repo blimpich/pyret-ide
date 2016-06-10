44a54f2dae6bef448ccc6e51e5991b40ff5e0744
Benjamin Limpich <benlimpich@gmail.com>
2016-06-09 17:11:33 -0700

    added buttons to toolbar and the start of a dropdown menu

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,12 +1,35 @@
 import React from "react";
+import Menu from "react-menus";
+
+//.button {}
+
+//.buttonRun {background-color: #317BCF}
+
+function onClick(events, props, index){}
 
 const toolbarStyle = {color: "white", backgroundColor: "gray", width: "100%", height: 40, position: "fixed"};
+
+const items = [
+  {
+    label: "Download This File",
+    onClick: function(itemProps, index, event) {
+      console.log('well,hello')
+    }
+  }
+];
+
+//<Menu items={items} onClick={onClick}/>
 
 export default class Toolbar extends React.Component {
   render() {
     return (
       <div style={toolbarStyle}> 
         <img style={{height: "100%"}} src={this.props.logo}/>
+        <button style={{height: "100%"}}>Connect to Google Drive</button>
+        <button style={{height: "100%"}}>More</button>
+        <Menu items={items} onClick={onClick}/>
+        <button style={{height: "100%"}}>Run</button>
+        <button style={{height: "100%"}}>Stop</button>
         this is the toolbar!
       </div>
     );

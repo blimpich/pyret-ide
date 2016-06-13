89c3d09194bec93f2f2323c3d6935d60a05c3fd6
Benjamin Limpich <benlimpich@gmail.com>
2016-06-13 14:21:28 -0700

    changed font of stop button to a gray instead of a black color to better replicate the original

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -45,6 +45,21 @@ const toolbarStyle = {color: "white", backgroundColor: "#C0C0C0", width: "100%",
  *   }
  * }*/
 
+/* class DropDownMenu extends React.Component {
+ *   constructor(props) {
+ *     super(props);
+ *     this.state = {expanded: false};
+ *   }
+ *   render() {
+ *     return(
+ *       <div>
+ *          <button onClick={()=>{this.setState({expanded: true});}}>More</button>
+ *          {this.state.expanded ? <Menu items={items} onClick={onClick}/> : null}
+ *       </div>
+ *     );
+ *   }
+ * }*/
+
 export default class Toolbar extends React.Component {
   render() {
     return (
@@ -53,7 +68,7 @@ export default class Toolbar extends React.Component {
       <a href="https://code.pyret.org/"><img style={{height: "80%", paddingLeft: 15, paddingRight: 15, paddingTop: 5, display: "block", float: "left"}} src={this.props.logo}/></a>
       <button style={{height: "100%", border: "none", fontSize:"15", fontFamily: "sans-serif", width: 194, backgroundColor: "gray", color: "white"}}>Connect to Google Drive</button>
       <button style={{height: "100%", border: "none", fontSize:"15", fontFamily: "sans-serif", width: 75.88, backgroundColor: "gray", color: "white", marginLeft: "3px"}}>More</button>
-      <button style={{height: "100%", border: "none", fontSize:"15", fontFamily: "sans-serif", width: 150, float: "right"}}>Stop</button>
+      <button style={{height: "100%", border: "none", fontSize:"15", fontFamily: "sans-serif", width: 150, float: "right", color: "gray"}}>Stop</button>
       <button style={{height: "100%", border: "none", fontSize:"15", fontFamily: "sans-serif", width: 150, float: "right", backgroundColor: "#317BCF", color: "white"}}>Run</button>
 
       </div>

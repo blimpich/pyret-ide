2e94e8ba167538b9ed5670ff295a165547e05d2a
Benjamin Limpich <benlimpich@gmail.com>
2016-06-13 17:08:59 -0700

    began writing react for static toolbar, previous version was naively and incorrectly written

--- src/components/PyretIDE.js
+++ src/components/PyretIDE.js
@@ -6,8 +6,7 @@ export default class PyretIDE extends React.Component {
   render() {
     return (
       <div>
-        <Toolbar logo="https://code.pyret.org/img/pyret-logo.png"/>
-	The Pyret IDE!
+          <Toolbar logo="https://code.pyret.org/img/pyret-logo.png" gif="https://code.pyret.org/img/pyret-spin.gif"/>
       </div>
     );
   }
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,76 +1,100 @@
 import React from "react";
 import Menu from "react-menus";
 
-//function onClick(events, props, index) {}
+function onClick(events, props, index) {}
 //function onChildClick(event, props, index) {}
 
 const toolbarStyle = {color: "white", backgroundColor: "#C0C0C0", width: "100%", height: 40, position: "fixed"};
 
-//var itemProps = {};
-//expander: "Sub Menu"
+/* var itemProps = {};
+ * expander: "Sub Menu"
+ * */
+const items = [
+  {
+    label: "Download this file"
+  },
+  {
+    label: "Documentation"
+  },
+  {
+    label: "TODO: Font"
+  },
+  {
+    label: "Report an Error"
+  },
+  {
+    label: "Discuss Pyret"
+  },
+  {
+    label: "Log Out"
+  }
+];
 
-/* const items = [
- *   {
- *     label: "Download this file"
- *   },
- *   {
- *     label: "Documentation"
- *   },
- *   {
- *     label: "TODO: Font"
- *   },
- *   {
- *     label: "Report an Error"
- *   },
- *   {
- *     label: "Discuss Pyret"
- *   },
- *   {
- *     label: "Log Out"
- *   }
- * ]; */
+//TODO
+class Connect2GDrive extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return(
+      <button style={{height: "100%", border: "none", fontSize:"15px", fontFamily: "sans-serif", width: 194, backgroundColor: "gray", color: "white"}}>Connect to Google Drive</button>
+    );
+  }
+}
 
-/* class DropDownMenu extends React.Component {
- *   constructor(props) {
- *     super(props);
- *     this.state = {expanded: false};
- *   }
- *   render() {
- *     return(
- *       <div>
- *       <button onClick={()=>{this.setState({expanded: true});}}>More</button>
- *       {this.state.expanded ? <Menu items={items} onClick={onClick}/> : null}
- *       </div>
- *     );
- *   }
- * }*/
+class StopButton extends React.Component {
+  constructor(props){
+    super(props);
+    var normalStopStyle = {height: "100%", border: "none", fontSize:"15px", fontFamily: "sans-serif", width: 150, float: "right", color: "gray"};
+    var runningStopStyle = {height: "100%", border: "none", fontSize:"15px", fontFamily: "sans-serif", width: 150, float: "right", color: "gray", backgroundColor: "#FF0000"};
+  }
+  render() {
+    return(
+      {this.state.running ? <button style={runningStopStyle}>Stop</button> : <button style={normalStopStyle}>Stop</button>}
+    );
+  }
+}
 
-/* class DropDownMenu extends React.Component {
- *   constructor(props) {
- *     super(props);
- *     this.state = {expanded: false};
- *   }
- *   render() {
- *     return(
- *       <div>
- *          <button onClick={()=>{this.setState({expanded: true});}}>More</button>
- *          {this.state.expanded ? <Menu items={items} onClick={onClick}/> : null}
- *       </div>
- *     );
- *   }
- * }*/
+
+//TODO
+class RunButton extends React.Component {
+  constructor(props){
+    super(props);
+    var normalRunStyle = {height: "100%", border: "none", fontSize:"15px", fontFamily: "sans-serif", width: 150, float: "right", backgroundColor: "#317BCF", color: "white"};
+    var runningRunStyle = {height: "100%", border: "none", fontSize:"15px", fontFamily: "sans-serif", width: 150, float: "right", color: "gray"};
+  }
+  render() {
+    return(
+      {this.state.running ? <button style={runningRunStyle}><img src={this.props.gif}/>Running...</button> : <button style={normalRunStyle}>Run</button> } 
+    );
+  }
+}
+
+//TODO
+class DropDownMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {expanded: false};
+  }
+  render() {
+    return(
+      <div>
+          <button style={{height: "100%", border: "none", fontSize:"15px", fontFamily: "sans-serif", width: 75.88, backgroundColor: "gray", color: "white", marginLeft: "3px"}} onClick={()=>{this.setState({expanded: true});}}>More</button>
+          {this.state.expanded ? <Menu items={items} onClick={onClick}/> : null}
+      </div>
+    );
+  }
+}
 
 export default class Toolbar extends React.Component {
   render() {
     return (
       <div style={toolbarStyle}>
-
-      <a href="https://code.pyret.org/"><img style={{height: "80%", paddingLeft: 15, paddingRight: 15, paddingTop: 5, display: "block", float: "left"}} src={this.props.logo}/></a>
-      <button style={{height: "100%", border: "none", fontSize:"15", fontFamily: "sans-serif", width: 194, backgroundColor: "gray", color: "white"}}>Connect to Google Drive</button>
-      <button style={{height: "100%", border: "none", fontSize:"15", fontFamily: "sans-serif", width: 75.88, backgroundColor: "gray", color: "white", marginLeft: "3px"}}>More</button>
-      <button style={{height: "100%", border: "none", fontSize:"15", fontFamily: "sans-serif", width: 150, float: "right", color: "gray"}}>Stop</button>
-      <button style={{height: "100%", border: "none", fontSize:"15", fontFamily: "sans-serif", width: 150, float: "right", backgroundColor: "#317BCF", color: "white"}}>Run</button>
-
+          <a href="https://code.pyret.org/"><img style={{height: "80%", paddingLeft: 15, paddingRight: 15, paddingTop: 5, display: "block", float: "left"}} src={this.props.logo}/></a>
+          <DropDownMenu/>
+          <RunButton/>
+          <StopButton/>
+          <Connect2GDrive/>
       </div>
     );
   } 
@@ -79,4 +103,4 @@ export default class Toolbar extends React.Component {
 Toolbar.propTypes = {logo: React.PropTypes.string};
 
 //<button style={{height: "100%", backgroundColor: "gray", color: "white"}}</button>
-//<DropDownMenu/> needs to be placed up in the render function
+

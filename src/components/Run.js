a57be738086df21f9e967f308a84aebe26bb052d
Benjamin Limpich <benlimpich@gmail.com>
2016-06-15 10:44:31 -0700

    revised ternary expresion to an if else expression, added Run.proptype for pyret gif

--- src/components/Run.js
+++ src/components/Run.js
@@ -1,20 +1,33 @@
 import React from "react";
 import Radium from "radium";
-import styles from "styles";
+import styles from "./styles";
 
 
 //TODO
 //if clicked this.setState should make running equal to true
 //test and (probably) debug gif img within img
 @Radium
-class RunButton extends React.Component {
+export default class Run extends React.Component {
   constructor(props){
     super(props);
     this.state = {running: false};
   }
   render() {
-    return(
-      {this.state.running ? <button style={[styles.buttons.base, styles.buttons.run.running]}><img src={this.props.gif}/>Running...</button> : <button style={[styles.button.base, styles.buttons.run.notRunning]}>Run</button>} 
-    );
+    if (this.state.running) {
+      return (
+        <button style={[styles.buttons.base, styles.buttons.run.running]}>
+            <img src={this.props.gif}/>
+            Running...
+        </button>
+      );
+    } else {
+      return (
+        <button style={[styles.button.base, styles.buttons.run.notRunning]}>
+            Run
+        </button>
+      );
+    }
   }
 }
+
+Run.propTypes = {gif: React.PropTypes.string};

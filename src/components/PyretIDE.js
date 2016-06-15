d0f1829c8e25f15a5d4ef62c670f7ee3a197ad75
Benjamin Limpich <benlimpich@gmail.com>
2016-06-15 10:33:58 -0700

    fixed over-indented html line

--- src/components/PyretIDE.js
+++ src/components/PyretIDE.js
@@ -1,13 +1,11 @@
 import React from 'react';
-
-// import Toolbar from './Toolbar';
-import Home from './Home';
+import Toolbar from './Toolbar';
 
 export default class PyretIDE extends React.Component {
   render() {
     return (
       <div>
-          <Toolbar logo="https://code.pyret.org/img/pyret-logo.png" gif="https://code.pyret.org/img/pyret-spin.gif"/>
+        <Toolbar logo="https://code.pyret.org/img/pyret-logo.png" gif="https://code.pyret.org/img/pyret-spin.gif"/>
       </div>
     );
   }

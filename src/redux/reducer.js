46cc4ccfd3b899fcc3df067aa87854c67a9a8d83
Richard Farman <farmanrl@whitman.edu>
2016-06-28 15:13:03 -0700

    Add reducer for run() actions

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,15 +1,25 @@
 import * as actType from './action-types';
+import {combineReducers} from 'redux';
 
-const initialState = {
+const initialStateApi = {
   loadingApi: undefined,
   runtimeApi: undefined,
   error: undefined
 };
 
-function loadApi(state = initialState, action) {
+const initialStateRun = {
+  runtimeStage: undefined,
+  ast: undefined,
+  bytecode: undefined,
+  result: undefined,
+  error: undefined,
+  pausing: false
+};
+
+function loadApi(state = initialStateApi, action) {
   switch (action.type) {
     case actType.START_LOAD_RUNTIME:
-      return Object.assign({}, state, {loadingApi: 'started'});
+      return Object.assign({}, state, {loadingApi: 'starting'});
     case actType.FINISH_LOAD_RUNTIME:
       return Object.assign({}, state, {loadingApi: 'finished', runtimeApi: action.payload});
     case actType.FAIL_LOAD_RUNTIME:
@@ -19,4 +29,40 @@ function loadApi(state = initialState, action) {
   }
 }
 
-export default loadApi;
+function runCode(state = initialStateRun, action) {
+  switch (action.type) {
+    case actType.START_PARSE:
+      return Object.assign({}, state, {runtimeStage: 'parsing'});
+    case actType.FINISH_PARSE:
+      return Object.assign({}, state, {ast: action.payload});
+    case actType.FAIL_PARSE:
+      return Object.assign({}, state, {error: action.payload});
+    case actType.START_COMPILE:
+      return Object.assign({}, state, {runtimeStage: 'compiling'});
+    case actType.FINISH_COMPILE:
+      return Object.assign({}, state, {bytecode: action.payload});
+    case actType.FAIL_COMPILE:
+      return Object.assign({}, state, {error: action.payload});
+    case actType.START_EXECUTE:
+      return Object.assign({}, state, {runtimeStage: 'executing'});
+    case actType.FINISH_EXECUTE:
+      return Object.assign({}, state, {result: action.payload});
+    case actType.FAIL_EXECUTE:
+      return Object.assign({}, state, {error: action.payload});
+    case actType.STOP_RUN:
+      return Object.assign({}, state, {runtimeStage: 'stopping'});
+    case actType.PAUSE_RUN:
+      return Object.assign({}, state, {pausing: true});
+    case actType.CLEAR_STATE:
+      return Object.assign({}, initialStateRun);
+    default:
+      return state;
+  }
+}
+
+const pyretReducer = combineReducers({
+  loadApi,
+  runCode
+});
+
+export default pyretReducer;
